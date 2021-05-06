21a3e46c4d0728476f6c1f5f5fdb109abb69a8c5
CharleyDvs <ing.carlosjaime@outlook.com>
2021-05-06 13:02:14 -0500

    Modified the app component to fetch all the characters data once mounted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { useState, useEffect } from "react"
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [characterList, setCharacterList] = useState([])
+
+  const fetchCharacters = async (url) => {
+    try {
+      const response = await fetch(url)
+      const { results, next } = await response.json()
+      return { results, next }
+    } catch (err) {
+      console.warn("Error", err)
+    }
+  }
+
+  const fetchAllCharacters = async (url, allCharacters = []) => {
+    setIsLoading(true)
+    try {
+      const { results, next } = await fetchCharacters(url)
+      allCharacters = [...allCharacters, ...results]
+      if (next !== null) {
+        return fetchAllCharacters(next, allCharacters)
+      }
+      setCharacterList(allCharacters)
+      setIsLoading(false)
+    } catch (err) {
+      console.warn(err)
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchAllCharacters("https://swapi.dev/ap/people/")
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {isLoading && <h1>Loading...</h1>}
+      {characterList.length > 1 && (
+        <ul>
+          {characterList.map((character) => (
+            <li key={character.name + character.birthyear}>{character.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
